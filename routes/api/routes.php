7ee9cc6412b50cd2a07f694dc95986b55d372946
Arthur Monney <monneylobe@gmail.com>
2023-06-30 13:58:00 +0200

    Setup API routes

--- routes/api/routes.php
+++ routes/api/routes.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', static fn () => ['message' => 'Welcome to the API!']);
+Route::get('/', static fn () => ['message' => 'Welcome to the API !']);
 
 Route::prefix('v1')->as('v1:')->group(
     base_path('routes/api/v1/api.php'),
--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -1,3 +1,7 @@
 <?php
 
 declare(strict_types=1);
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', static fn () => ['message' => 'Welcome to the API version 1.0 !'])->name('home');
