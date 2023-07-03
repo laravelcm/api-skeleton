a9170f0bc6b00f67e8a89f4513d2396c7fd6442c
Arthur Monney <monneylobe@gmail.com>
2023-07-03 10:55:22 +0200

    Change default home api route

--- lang/en/messages.php
+++ lang/en/messages.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 return [
-    'service' => [
-        'online' => 'Service Online',
-    ],
+
+    'welcome' => 'Welcome to Laravel API Skeleton',
+
 ];
--- routes/api/routes.php
+++ routes/api/routes.php
@@ -2,9 +2,10 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', static fn () => ['message' => 'Welcome to the API !']);
+Route::get('/', HomeController::class);
 
 Route::prefix('v1')->as('v1:')->group(
     base_path('routes/api/v1/api.php'),
