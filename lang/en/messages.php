5dd9cecd581efa440c3075dff6eb73b3541d1587
Arthur Monney <monneylobe@gmail.com>
2023-07-04 07:44:41 +0200

    Setup default API Skeleton

--- lang/en/messages.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-
-    'welcome' => 'Welcome to Laravel API Skeleton',
-
-];
--- routes/api/v1/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', static fn () => ['message' => 'Welcome to the API version 1.0 !'])->name('home');
