600526a3f2a9596cdf706400881af5e9764f0cd8
karimalik <me@karimdev.network>
2023-12-25 12:34:16 +0100

    add passport package

---
+++ core/Exceptions/Handler.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Core\Exceptions;
+
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
+
+final class Handler extends ExceptionHandler
+{
+    protected $levels = [];
+
+    protected $dontReport = [];
+
+    protected $dontFlash = [
+        'current_password',
+        'password',
+        'password_confirmation',
+    ];
+
+    public function register(): void
+    {
+        $this->reportable(function (Throwable $e) {
+            //
+        });
+    }
+}
